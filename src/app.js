185bcea701abd7feb74c7ef5351793b0f748fa67
NecroBread <mariovyordanov@gmail.com>
2022-03-19 16:35:39 +0200

    Router Setup

--- src/app.js
+++ src/app.js
@@ -19,34 +19,5 @@ import './pages/app-root.js'
 // CSS
 import '../static/app.css';
 
-// LIBRARIES
-import { LitElement, css, html, render } from 'lit';
-import page from 'page';
-
-// CONSTANTS
-const outlet = document.getElementById('outlet');
-
 // ROUTER
-page('/', () => render(html`
-<app-root>
-	<home-page></home-page>
-</app-root>`, outlet));
-page('/profile', () => render(html`<app-root>
-	<profile-page></profile-page>
-</app-root>`, outlet));
-page('/network', () => render(html`<app-root>
-	<network-page></network-page>
-</app-root>`, outlet));
-page('/search', () => render(html`<app-root>
-	<search-page></search-page>
-</app-root>`, outlet));
-page('/settings', () => render(html`<app-root>
-	<settings-page></settings-page>
-</app-root>`, outlet));
-page('/login', () => render(html`<app-root>
-	<login-page></login-page>
-</app-root>`, outlet));
-page('/register', () => render(html`<app-root>
-	<register-page></register-page>
-</app-root>`, outlet));
-page.start();
+import './router.js'
---
+++ src/router.js
@@ -0,0 +1,34 @@
+import { html, render } from 'lit';
+import page from 'page';
+
+const outlet = document.getElementById('outlet');
+
+page('/', () => render(html`
+<app-root>
+	<home-page></home-page>
+</app-root>`, outlet));
+
+page('/profile', () => render(html`<app-root>
+	<profile-page></profile-page>
+</app-root>`, outlet));
+
+page('/network', () => render(html`<app-root>
+	<network-page></network-page>
+</app-root>`, outlet));
+
+page('/search', () => render(html`<app-root>
+	<search-page></search-page>
+</app-root>`, outlet));
+
+page('/settings', () => render(html`<app-root>
+	<settings-page></settings-page>
+</app-root>`, outlet));
+
+page('/login', () => render(html`<app-root>
+	<login-page></login-page>
+</app-root>`, outlet));
+
+page('/register', () => render(html`<app-root>
+	<register-page></register-page>
+</app-root>`, outlet));
+page.start();
