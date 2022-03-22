906473da01923d10f05c66fc1939f571f5c65efc
NecroBread <mariovyordanov@gmail.com>
2022-03-22 20:14:44 +0200

    More work on nav

--- src/components/main-nav.js
+++ src/components/main-nav.js
@@ -4,6 +4,7 @@ import { resets } from '../components-css/resets';
 
 class MainNav extends LitElement {
 	static properties = {
+		isLogged: {type: Boolean},
 		activePage: { type: String },
 	}
 
@@ -63,9 +64,6 @@ class MainNav extends LitElement {
 		width: 23px;
 		height: 23px;
 	} 
-	.danger {
-		color: red;
-	}
 
 	/* Hamburger Menu */
 	#hamburger-icon {
@@ -129,6 +127,18 @@ class MainNav extends LitElement {
 		text-decoration: none;
 	}
 
+	.mobile-menu a.active {
+		text-decoration: underline;
+	}
+
+	.mobile-menu a:hover {
+		color: orange;
+	}
+
+	.mobile-menu a.danger {
+		color: red;
+	}
+
 	@media only screen and (max-width: 980px) {
 	
 		#wrapper {
@@ -150,18 +160,114 @@ class MainNav extends LitElement {
 
 	constructor() {
 		super();
-		this.activePage = '/profile';
-	}
-
-	toggleHamburger(e) {
-		e.preventDefault();
-		console.log(e.target);
-		e.currentTarget.classList.toggle('open');
+		this.isLogged = true;
+		this.activePage = '/';
 	}
 
 	isActive(page) {
 		return this.activePage === page;
 	}
+
+	mainNavTemplate(isLogged) {
+		return html`
+			<div id="navbar">
+				<ul>
+					${isLogged 
+					? html`<!-- Home -->
+					<li><a href="/" class=${classMap({ active: this.isActive('/') })}> <svg xmlns="http://www.w3.org/2000/svg"
+								class="bi bi-house" viewBox="0 0 16 16">
+								<path fill-rule="evenodd"
+									d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
+								<path fill-rule="evenodd"
+									d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z" />
+							</svg>
+						</a></li>
+					<!-- Profile -->
+					<li><a href="/profile" class=${classMap({ active: this.isActive('/profile') })}>
+							<svg xmlns="http://www.w3.org/2000/svg" class="bi bi-person" viewBox="0 0 16 16">
+								<path
+									d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+							</svg>
+						</a></li>
+					<!-- Circles -->
+					<li><a href="/circles" class=${classMap({ active: this.isActive('/circles') })}>
+							<svg xmlns="http://www.w3.org/2000/svg" class="bi bi-people" viewBox="0 0 16 16">
+								<path
+									d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z" />
+							</svg>
+						</a></li>
+					<!-- Search -->
+					<li><a href="/search" class=${classMap({ active: this.isActive('/search') })}>
+							<svg xmlns="http://www.w3.org/2000/svg" class="bi bi-search" viewBox="0 0 16 16">
+								<path
+									d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+							</svg>
+						</a></li>
+					<!-- Settings -->
+					<li><a href="/settings" class=${classMap({ active: this.isActive('/settings') })}>
+							<svg xmlns="http://www.w3.org/2000/svg" class="bi bi-gear" viewBox="0 0 16 16">
+								<path
+									d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z" />
+								<path
+									d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z" />
+							</svg>
+						</a>
+					</li>`
+					: html`
+					<!-- Register -->
+					<li><a href="/register">
+							<svg xmlns="http://www.w3.org/2000/svg" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+								<path fill-rule="evenodd"
+									d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z" />
+								<path fill-rule="evenodd"
+									d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+							</svg>
+						</a>
+					</li>`
+					}
+				</ul>
+			</div>
+		`
+	}
+
+	hamburgerTemplate(isLogged) {
+		return html`
+			<div id="hamburger-icon" @click=${this.toggleHamburger}>
+					<div class="bar1"></div>
+					<div class="bar2"></div>
+					<div class="bar3"></div>
+					<ul class="mobile-menu">
+						${isLogged 
+						? html`<!-- Home -->
+						<li><a href="/" class=${classMap({ active: this.isActive('/') })}>Home</a></li>
+						<!-- Profile -->
+						<li><a href="/profile" class=${classMap({ active: this.isActive('/profile') })}>Profile</a></li>
+						<!-- Circles -->
+						<li><a href="/circles" class=${classMap({ active: this.isActive('/circles') })}>Circles</a></li>
+						<!-- Search -->
+						<li><a href="/search" class=${classMap({ active: this.isActive('/search') })}>Search</a></li>
+						<!-- Settings -->
+						<li><a href="/settings" class=${classMap({ active: this.isActive('/settings') })}>Settings</a></li>
+						<li><a class="danger" href="#">Logout</a></li>`
+						: html`
+						<!-- Login -->
+						<li><a href="/login" class=${classMap({ active: this.isActive('/login') })}>Login</a></li>
+						<!-- Register -->
+						<li><a href="/register" class=${classMap({ active: this.isActive('/register') })}>Register</a></li>
+						`}
+					</ul>
+				</div>
+		`
+	}
+
+	toggleHamburger(e) {
+		e.currentTarget.classList.toggle('open');
+	}
+
+
+
+
+
 
 	render() {
 		return html`
@@ -170,68 +276,8 @@ class MainNav extends LitElement {
 				<a id="logo" href="/"><img src="https://i.ibb.co/0VB99wP/logo-color.png" alt="Schmoozer"></a>
 			</div>
 			<nav>
-				<div id="navbar">
-					<ul>
-						<!-- Home -->
-						<li><a href="/" class=${classMap({ active: this.isActive('/') })}> <svg
-									xmlns="http://www.w3.org/2000/svg" class="bi bi-house" viewBox="0 0 16 16">
-									<path fill-rule="evenodd"
-										d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
-									<path fill-rule="evenodd"
-										d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z" />
-								</svg>
-							</a></li>
-						<!-- Profile -->
-						<li><a href="/profile" class=${classMap({ active: this.isActive('/profile') })}>
-								<svg xmlns="http://www.w3.org/2000/svg" class="bi bi-person" viewBox="0 0 16 16">
-									<path
-										d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
-								</svg>
-							</a></li>
-						<!-- Circles -->
-						<li><a href="/circles" class=${classMap({ active: this.isActive('/circles') })}>
-								<svg xmlns="http://www.w3.org/2000/svg" class="bi bi-people" viewBox="0 0 16 16">
-									<path
-										d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z" />
-								</svg>
-							</a></li>
-						<!-- Search -->
-						<li><a href="/search" class=${classMap({ active: this.isActive('/search') })}>
-								<svg xmlns="http://www.w3.org/2000/svg" class="bi bi-search" viewBox="0 0 16 16">
-									<path
-										d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-								</svg>
-							</a></li>
-						<!-- Settings -->
-						<li><a href="/settings" class=${classMap({ active: this.isActive('/settings') })}>
-								<svg xmlns="http://www.w3.org/2000/svg" class="bi bi-gear" viewBox="0 0 16 16">
-									<path
-										d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z" />
-									<path
-										d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z" />
-								</svg>
-							</a></li>
-		
-					</ul>
-				</div>
-				<div id="hamburger-icon" @click=${this.toggleHamburger}>
-					<div class="bar1"></div>
-					<div class="bar2"></div>
-					<div class="bar3"></div>
-					<ul class="mobile-menu">
-						<!-- Home -->
-						<li><a href="/">Home</a></li>
-						<!-- Profile -->
-						<li><a href="/profile">Profile</a></li>
-						<!-- Circles -->
-						<li><a href="/circles">Circles</a></li>
-						<!-- Search -->
-						<li><a href="/search">Search</a></li>
-						<!-- Settings -->
-						<li><a href="/settings">Settings</a></li>
-						<li><a class="danger" href="#">Logout</a></li>
-					</ul>
-				</div>
+				${this.mainNavTemplate(this.isLogged)}
+				${this.hamburgerTemplate(this.isLogged)}
 			</nav>
 		</div>
 		`
