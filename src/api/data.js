fb8988ff86874c94360e8bdd77dab61765f9f096
NecroBread <mariovyordanov@gmail.com>
2022-04-09 16:54:33 +0300

    Added pagination to posts requests

--- src/api/data.js
+++ src/api/data.js
@@ -1,8 +1,10 @@
 import { getUserData } from '../utils/userData';
 import * as db from './db';
 
-export const getAllPosts = () => db.get(`/classes/Post?include=creator`);
-export const getPostsByUserId = (id) => db.get(`/classes/Post?where={"creator":{"__type":"Pointer","className":"_User","objectId":"${id}"}}&include=creator`);
+export const login = db.login;
+export const register = db.register;
+export const logout = db.logout;
+
 export const getDetails = (id) => db.get(`/classes/Post?where={"objectId":"${id}"}`);
 export const getUserInfoById = (id) => db.get(`/users/${id}`)
 export const newPost = (data) => {
@@ -10,9 +12,20 @@ export const newPost = (data) => {
 	return db.post('/classes/Post', data)
 };
 
-export const login = db.login;
-export const register = db.register;
-export const logout = db.logout;
+
+export const getAllPosts = (page) => {
+	const limit = 2;
+	const skip = limit * page - 1;
+	return db.get(`/classes/Post?include=creator&order=-createdAt&limit=${limit}&skip=${skip}`);
+};
+
+export const getPostsByUserId = (id, page) => {
+	const limit = 2;
+	const skip = limit * page - 1;
+
+	return db.get(`/classes/Post?where={"creator":{"__type":"Pointer","className":"_User","objectId":"${id}"}}&include=creator&order=-createdAt&limit=${limit}&skip=${skip}`);
+}
+
 
 
 function createPointer(name, id) {
