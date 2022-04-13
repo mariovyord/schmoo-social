06a4717220abe2522e5f2dc1a266eca80ee2cea0
NecroBread <mariovyordanov@gmail.com>
2022-04-13 18:54:36 +0300

    Minor cleanup

--- src/middleware/renderNav.js
+++ src/middleware/renderNav.js
@@ -1,7 +1,6 @@
-import { LitElement, css, html, render } from 'lit';
+import { html, render } from 'lit';
 
 export default function renderNav(ctx, next) {
-	console.log(ctx);
 	render(html`
 		<main-nav .ctx=${ctx}></main-nav>`, document.querySelector('header'));
 
